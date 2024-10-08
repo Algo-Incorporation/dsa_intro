8ff2ec2f0b2926a1766d41e4175c425119ca82a9
Aiclys <145050105+Aiclys@users.noreply.github.com>
2024-10-08 09:16:10 +0200

    create method templates in Stack.java

--- src/data-structures/Stack.java
+++ src/data-structures/Stack.java
@@ -1,3 +1,34 @@
 public class Stack {
+    int size;
+    int[] stackArr;
+    int topElement;
 
-}
+    public Stack(int size) {
+        this.size = size;
+        stackArr = new int[size]; 
+        topElement = -1;
+    }
+
+    public void push(int value) {
+        if (topElement == size - 1) {
+            System.out.println("Stack overflow");
+            return;
+        }
+        stackArr[++topElement] = value;
+        System.out.println(value + " pushed into the stack");
+
+    }
+
+    public void pop() {
+
+    }
+
+    public void top() {
+
+    }
+
+    public void isEmpty() {
+
+    }
+} 
+
